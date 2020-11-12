25481193fb74e94223d44889ad4f3b907f5a0d67
jasmeets7 <sing6443@sheridancollege.ca>
2020-11-12 01:16:13 -0500

    Added Chat System v1.3

--- controllers/messaging/chat.js
+++ controllers/messaging/chat.js
@@ -16,6 +16,12 @@ exports.sendMessage = (req, res, next) => {
 
     let time = req.body.time;
 
+    if (message === undefined || message === "" || message === null) {
+        return res.status(401).json({
+            message: "Enter some value before sending message"
+        });
+    }
+
     Chat.findOne({ _id: room}).then((result)=>{
         if (!result) {
             const chat = new Chat({
@@ -71,61 +77,6 @@ exports.getMessages = (req, res, next) => {
             result: result
         });
     })
-
-}
-
-
-exports.sendMessage = (req, res, next) => {
-
-    let messageID =  req.body.messageID;
-
-    let user = req.body.user;
-
-    let username = req.body.username;
-
-    let room = req.body.room;
-
-    let message = req.body.message;
-
-    let time = req.body.time;
-
-    Chat.findOne({ _id: room}).then((result)=>{
-        if (!result) {
-            const chat = new Chat({
-                _id: room,
-                conversation: [
-                    {
-                        messageID : messageID,
-                        user : user,
-                        username : username,
-                        message : message,
-                        time: time
-                    }
-                ]
-            });
-            chat.save().then(result => {
-                res.status(201).json({
-                    result: result
-                });
-            }).catch(err => {
-                res.status(400).json({
-                    message: err
-                });
-            });
-        } else {
-            Chat.updateOne({ _id: room }, {$push: { conversation: { messageID : messageID, user : user, username : username, message : message, time: time } }}).then((result)=>{
-                // console.log(result);
-            }).catch(err => {
-                res.status(400).json({
-                    message: err
-                });
-            });
-        }
-    }).catch(err => {
-        res.status(400).json({
-            message: err
-        });
-    });
 
 }
 
